33a64602fe05b1f3dd0c9775a7ad42ab0f96d51b
Shounak <shounak.b95@gmail.com>
2020-11-10 20:08:38 -0800

    make it better

---
+++ webapp.py
@@ -0,0 +1,119 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+from keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import math
+
+app = dash.Dash()
+server = app.server
+
+df=pd.read_csv("AppleData.csv")
+df["Date1"]=pd.to_datetime(df.Date1,format="%m/%d/%Y")
+df.index=df['Date1']
+#sorting and filtering the data
+data=df.sort_index(ascending=True,axis=0)
+new_dataset=pd.DataFrame(index=range(0,len(df)),columns=['Date','Close'])
+for i in range(0,len(data)):
+    new_dataset["Date"][i]=data['Date1'][i]
+    new_dataset["Close"][i]=data["Close"][i]
+#normalizing the data
+final_dataset=new_dataset.values
+new_dataset.index=new_dataset.Date
+new_dataset.drop("Date",axis=1,inplace=True)
+final_dataset=new_dataset.values
+value_of_len=math.ceil(len(new_dataset)*0.8)
+train_data=final_dataset[0:value_of_len,:]#breaking the data into traning and validation
+valid_data=final_dataset[value_of_len:,:]
+scaler=MinMaxScaler(feature_range=(0,1))#creating a variable to scale the data on its features between 0 and 1
+scaled_data=scaler.fit_transform(final_dataset)#transforming the data and giving it values between 0 and 1 based on its featres
+
+a_train_data=[]
+b_train_data=[]
+
+for i in range(10,value_of_len):
+    a_train_data.append(scaled_data[i-10:i,0])#will contain the first 50 vales from scaled data
+    b_train_data.append(scaled_data[i,0])#will contain the 51st value
+
+a_train_data=np.array(a_train_data)#converting the data into numpy arrays
+b_train_data=np.array(b_train_data)
+a_train_data=np.reshape(a_train_data,(a_train_data.shape[0],a_train_data.shape[1],1))
+
+
+model=load_model("saved_model.h5")
+
+inputs_data=new_dataset[len(new_dataset)-len(valid_data)-10:].values
+inputs_data=inputs_data.reshape(-1,1)
+inputs_data=scaler.transform(inputs_data)
+
+X_test=[]
+for i in range(10,inputs_data.shape[0]):
+    X_test.append(inputs_data[i-10:i,0])
+X_test=np.array(X_test)
+
+X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
+closing_price=model.predict(X_test)
+closing_price=scaler.inverse_transform(closing_price)
+
+train=new_dataset[:value_of_len]
+valid=new_dataset[value_of_len:]
+valid['Predictions']=closing_price
+
+app.layout = html.Div([
+   
+    html.H1("Stock Price Predictor ", style={"textAlign": "center"}),
+    dcc.Tabs(id="tabs", children=[
+            dcc.Tab(label='Apple',children=[
+                    html.Div([
+                        html.H2("Actual closing price",style={"textAlign": "center"}),
+                        dcc.Graph(
+                            id="Actual Data",
+                            figure={
+                                "data":[
+                                    go.Scatter(
+                                        x=valid.index,
+                                        y=valid["Close"],
+                                        mode='markers'
+                                    )
+                                ],
+                                "layout":go.Layout(
+                                    title='scatter plot',
+                                    xaxis={'title':'Date'},
+                                    yaxis={'title':'Closing Rate'}
+                                )
+                            }
+                        ),
+                        html.H2("LSTM Predicted closing price",style={"textAlign": "center"}),
+                        dcc.Graph(
+                            id="Predicted Data",
+                            figure={
+                                "data":[
+                                    go.Scatter(
+                                        x=valid.index,
+                                        y=valid["Predictions"],
+                                        mode='markers'
+                                    )
+                                ],
+                                "layout":go.Layout(
+                                    title='scatter plot',
+                                    xaxis={'title':'Date'},
+                                    yaxis={'title':'Closing Rate'}
+                                )
+                            }
+                        )                
+                    ])                
+                ]),
+            dcc.Tab(label='Facebook',children=[
+                 html.Div([
+                     html.H2("Actual closing price",style={"textAlign": "center"})
+                 ])
+            ])
+    ])
+])
+
+if __name__=='__main__':
+    app.run_server(debug=True)
